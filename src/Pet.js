98f7fe23d019bbda7d0572b0f6ffbe67c269e9c1
Amrit Thapa <thapa.amet27@gmail.com>
2021-12-21 22:58:57 +0545

    tool setup done

---
+++ src/Pet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Luna"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h3", {}, "Japenese Spintcher"),
+  ]);
+};
+
+export default Pet

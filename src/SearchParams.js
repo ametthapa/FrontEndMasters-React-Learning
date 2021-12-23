738eb826fa412fa7731c594d1418c993d9855058
Amrit Thapa <thapa.amet27@gmail.com>
2021-12-23 22:01:13 +0545

    useSatet used

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "raptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
@@ -14,6 +19,34 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
             placeholder="location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map(animal =>(
+              <option value={animal} key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map(breed =>(
+              <option value={breed} key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>

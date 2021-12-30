99a6629aa828809abe41a292552cf5101e4b9ca0
ametthapa <thapa.amet27@gmail.com>
2021-12-30 17:46:44 +0545

    fetched the data

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,26 @@
-const Details = () => {
-    return (
-        <div>This is deatils</div>
-    )
+import { Component } from "react-dom";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    constructor() {
+        super();
+
+        this.state = { loading: true };
+
+    }
+
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        )
+    }
+
+    render() {
+        return (
+            <h2>hi</h2>
+        )
+    }
 }
+
+
 export default Details;

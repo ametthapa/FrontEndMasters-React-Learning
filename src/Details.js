abd6ca8ac095ef6996cee760d1b2dc97d59ae0d6
Amrit Thapa <57837078+ametthapa@users.noreply.github.com>
2022-01-02 09:33:19 +0545

    Revert "error"

--- src/Details.js
+++ src/Details.js
@@ -6,30 +6,21 @@ class Details extends Component {
         super();
 
         this.state = { loading: true };
+
     }
 
     async componentDidMount() {
         const res = await fetch(
             `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
-        );
-        const json = await res.json();
-        this.setState(Object.assign({ loading: false }, json.pets[0]));
+        )
     }
 
     render() {
-        const { animal, breed, city, state, description, name } = this.state;
         return (
-            <div className="details">
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{animal} - {breed} -  {city}, {state}</h2>
-                    <button>Adopt {name}</button>
-                    <p>{description}</p>
-                </div>
-
-            </div>
+            <h2>hi</h2>
         )
     }
 }
 
-export default withRouter(Details);
+
+export default Details;

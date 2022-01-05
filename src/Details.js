20a002254d315bca4278d298c91ace6673f7d89f
ametthapa <thapa.amet27@gmail.com>
2022-01-05 16:20:23 +0545

    added all for now
    and its working

---
+++ src/Details.js
@@ -0,0 +1,16 @@
+// const Details = () => {
+//     return <h2>hi</h2>
+// }
+
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    render() {
+        return (
+            <h2>Hello</h2>
+        )
+    }
+}
+
+export default Details;  

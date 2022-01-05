8167a8324e59eeada01287d206e6fba83db53029
ametthapa <thapa.amet27@gmail.com>
2022-01-05 16:44:53 +0545

    done without details

--- src/Details.js
+++ src/Details.js
@@ -1,26 +1,5 @@
-import { Component } from "react-dom";
-import { withRouter } from "react-router-dom";
-
-class Details extends Component {
-    constructor() {
-        super();
-
-        this.state = { loading: true };
-
-    }
-
-    async componentDidMount() {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
-        )
-    }
-
-    render() {
-        return (
-            <h2>hi</h2>
-        )
-    }
+const Details = () => {
+    return <h2>Hello</h2>
 }
-
 
 export default Details;

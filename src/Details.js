cd310dd1b5d60bfae7c36bebc7fa044290f462d2
ametthapa <thapa.amet27@gmail.com>
2022-01-05 17:19:48 +0545

    done till details

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,37 @@
-const Details = () => {
-    return <h2>Hello</h2>
+// const Details = () => {
+//     return <h2>Hello</h2>
+// }
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    constructor() {
+        super();
+        this.state = { loading: true };
+    }
+
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json.pets[0])
+        );
+    }
+    render() {
+        const { animal, breed, name, description, city, state } = this.state;
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} -{city}, {state}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Details;
+
+export default withRouter(Details);

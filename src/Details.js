54ed95f192e9c7693c051775eb38e10ef72c2c1e
ametthapa <thapa.amet27@gmail.com>
2022-01-06 17:48:56 +0545

    all done
    except for button in details

--- src/Details.js
+++ src/Details.js
@@ -5,6 +5,7 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends Component {
     // constructor() {
@@ -37,7 +38,11 @@ class Details extends Component {
                 <div>
                     <h1>{name}</h1>
                     <h2>{animal} - {breed} -{city}, {state}</h2>
-                    <button>Adopt {name}</button>
+                    <ThemeContext.Consumer>
+                        {([theme]) => {
+                            <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+                        }}
+                    </ThemeContext.Consumer>
                     <p>{description}</p>
                 </div>
             </div>
